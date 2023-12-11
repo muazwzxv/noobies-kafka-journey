faaca2fdddc01cbf2904a38ec5a2861227c35936
Muaz Wazir <muazwazir@gmail.com>
2023-12-11 16:46:40 +0800

    setting high throughput configurations

--- kafka-producer-wikimedia/src/main/java/io/cilantroz/demo/kafka/producer/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/cilantroz/demo/kafka/producer/WikimediaChangesProducer.java
@@ -3,6 +3,7 @@ package io.cilantroz.demo.kafka.producer;
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -19,6 +20,16 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // set safe producer configs (kafka <= 2.8)
+        // properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        // properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        // properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        // setting high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
