166b11c4cadf76a8fcad742bc15db73b1724d121
Muaz Wazir <muazwazir@gmail.com>
2023-12-11 14:25:12 +0800

    setup wikimedia producer

---
+++ kafka-producer-wikimedia/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+}
+
+group = "io.cilantroz.demo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    //testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.6.1")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    // https://mvnrepository.com/artifact/com.launchdarkly/okhttp-eventsource
+    implementation("com.launchdarkly:okhttp-eventsource:4.1.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ kafka-producer-wikimedia/src/main/java/io/cilantroz/demo/kafka/producer/WikimediaChangesProducer.java
@@ -0,0 +1,7 @@
+package io.cilantroz.demo.kafka.producer;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+
+    }
+}
